0d6a86dc7b055d43de85b6af11457fae52e7a4b6
Richard Joffray <rjoffray@richards-mbp.lan>
2020-10-06 12:22:30 -0700

    optimized for AirTouch

--- src/public/js/config.js
+++ src/public/js/config.js
@@ -4,6 +4,43 @@ window.reload = function () {
   window.oak.reload()
 }
 
+function touchHandler(event)
+{
+    var touches = event.changedTouches,
+        first = touches[0],
+        type = "";
+    switch(event.type)
+    {
+        case "touchstart": type = "mousedown"; break;
+        case "touchmove":  type = "mousemove"; break;        
+        case "touchend":   type = "mouseup";   break;
+        case "touchcancel":   type = "mouseup";   break;
+        default:           return;
+    }
+
+    // initMouseEvent(type, canBubble, cancelable, view, clickCount, 
+    //                screenX, screenY, clientX, clientY, ctrlKey, 
+    //                altKey, shiftKey, metaKey, button, relatedTarget);
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+                                  first.screenX, first.screenY, 
+                                  first.clientX, first.clientY, false, 
+                                  false, false, false, 0/*left*/, null);
+
+    first.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+
+function init() 
+{
+    document.addEventListener("touchstart", touchHandler, { passive: false, capture: true });
+    document.addEventListener("touchmove", touchHandler, { passive: false, capture: true });
+    document.addEventListener("touchend", touchHandler, { passive: false, capture: true });
+    document.addEventListener("touchcancel", touchHandler, { passive: false, capture: true }); 
+
+}
+init()
 var app = window.angular
   .module('demoApp', ['ngMaterial', 'ngMessages'])
   .constant('oak', window.oak)
